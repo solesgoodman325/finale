99106303e14ad524affb88fa68383ada574e24fd
solesgoodman325 <1939957379@qq.com>
2025-12-18 00:49:13 +0800

    Add referer check to redirect users

---
+++ checkReferer.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 获取 Referer
+    const referer = document.referrer;
+
+    // 判断 Referer 是否包含 "fanbox"
+    if (referer.toLowerCase().includes("fanbox")) {
+        // 如果包含 "fanbox"，加载 download.html 页面
+        window.location.href = "download.html";
+    } else {
+        // 否则，跳转到 error 页面
+        window.location.href = "error.html";
+    }
+});
