986d83d0412157ce7a03dc5e356950cc88964592
solesgoodman325 <1939957379@qq.com>
2025-12-18 01:12:20 +0800

    Update checkReferer.js

--- checkReferer.js
+++ checkReferer.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // 获取 Referer
-    const referer = document.referrer;
 
-    // 判断 Referer 是否包含 "fanbox"
-    if (referer.toLowerCase().includes("fanbox")) {
-        // 如果包含 "fanbox"，加载 download.html 页面
-        window.location.href = "download.html";
-    } else {
-        // 否则，跳转到 error 页面
-        window.location.href = "error.html";
-    }
-});
